6943d4ea1fdf72bdd18c47b5b5f0bc6581c2ad69
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-06-21 19:38:12 +0300

    add abstarct class

--- courses/models.py
+++ courses/models.py
@@ -57,3 +57,18 @@ class Content(models.Model):
 
     class Meta:
         ordering = ['order']
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related',
+                              on_delete=models.CASCADE) 
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True 
+
+    def __str__(self):
+        return self.title
