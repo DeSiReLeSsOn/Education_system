c026002866bb7d62ac064dcef21325ddd492b55c
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-06-30 19:01:09 +0300

    add login and logged out

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, get_user_model, login, logout
 
-# Create your views here.
+def logout_user(request):
+    session_keys = list(request.session.keys())
+    for key in session_keys:
+        if key == 'session_key':
+            continue
+        del request.session[key]
+    logout(request)
+    return render(request, 'registration/logged_out.html')
--- education_system/urls.py
+++ education_system/urls.py
@@ -1,16 +1,21 @@
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views 
-
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from courses.views import logout_user
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'accounts/login/', auth_views.LoginView.as_view(), name='login'
+    ),
+    path(
+        'accounts/logout/', logout_user, name='logout'
+    ),
     path('admin/', admin.site.urls),
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
