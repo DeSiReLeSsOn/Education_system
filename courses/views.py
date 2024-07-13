1f6bfa9b4029420b44a341cdd76eb3d1096bcd0c
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-07-13 16:30:25 +0300

    create post method for update module

--- courses/views.py
+++ courses/views.py
@@ -155,6 +155,12 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
 
     
     def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form,
+                                        'object': self.obj})
+
+
+    def post(self, request, module_id, model_name, id=None):
         form = self.get_form(self.model,
                              instance=self.obj,
                              data=request.POST,
